e3a2dfdcff07f04409a62ca2b178fda8e55c1cd9
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-09-30 15:46:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,79 @@
 'use strict';
+
+function notify(message, type) {
+  const div = document.createElement('div');
+
+  div.dataset.qa = 'notification';
+  div.className = type;
+  div.textContent = message;
+
+  document.body.appendChild(div);
+
+  setTimeout(() => div.remove(), 4000);
+}
+
+// 1
+const firstPromise = new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-shadow
+  const clickHandler = (event) => {
+    if (event.button === 0) {
+      resolve('First promise was resolved on a left click in the document');
+      document.removeEventListener('mousedown', clickHandler);
+    }
+  };
+
+  document.addEventListener('mousedown', clickHandler);
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected in 3 seconds if not clicked'));
+  }, 3000);
+});
+
+firstPromise
+  .then((message) => notify(message, 'success'))
+  .catch((error) => notify(error.message, 'error'));
+
+// 2
+const secondPromise = new Promise((resolve) => {
+  // eslint-disable-next-line no-shadow
+  const clickHandler = (event) => {
+    if (event.button === 0 || event.button === 2) {
+      resolve('Second promise was resolved');
+      document.removeEventListener('mousedown', clickHandler);
+    }
+  };
+
+  document.addEventListener('mousedown', clickHandler);
+});
+
+secondPromise.then((message) => notify(message, 'success'));
+
+// 3
+const thirdPromise = new Promise((resolve) => {
+  let leftClicked = false;
+  let rightClicked = false;
+
+  // eslint-disable-next-line no-shadow
+  const clickHandler = (event) => {
+    if (event.button === 0) {
+      leftClicked = true;
+    }
+
+    if (event.button === 2) {
+      rightClicked = true;
+    }
+
+    if (leftClicked && rightClicked) {
+      resolve(
+        // eslint-disable-next-line max-len
+        'Third promise was resolved only after both left and right clicks happened',
+      );
+
+      document.removeEventListener('mousedown', clickHandler);
+    }
+  };
+
+  document.addEventListener('mousedown', clickHandler);
+});
+
+thirdPromise.then((message) => notify(message, 'success'));
